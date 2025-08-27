060c1258d9e95c6745757e8b12a736562e6793a6
dr1vesafe <snddadaya@gmail.com>
2025-08-27 14:39:35 +0400

    Создан файл database.py, в котором настроено подключение к базе даннных через SQLAlchemy

---
+++ src/app/database.py
@@ -0,0 +1,24 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from .config import settings
+
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=settings.DEBUG,
+    future=True
+)
+
+async_session = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
