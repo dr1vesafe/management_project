7fad90f27b387be83b41bcd428e8435e4782ba2c
dr1vesafe <snddadaya@gmail.com>
2025-08-28 14:11:10 +0400

    Созданы Pydantic схемы для команды Team

---
+++ src/app/schemas/team.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class TeamBase(BaseModel):
+    """Базовая схема команды"""
+    name: str
+
+
+class TeamCreate(TeamBase):
+    """Схема для создания команды"""
+    pass
+
+
+class TeamRead(TeamBase):
+    """Схема для получения данных команды"""
+    id: int
+    code: str
+
+    class Config:
+        from_attributes = True
+
+
+class TeamUpdate(BaseModel):
+    """Схема для обновления данных команды"""
+    name: Optional[str] = None
