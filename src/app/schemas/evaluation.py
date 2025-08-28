f0aa7cec906f397ba56e65efcb975da1a19d0139
dr1vesafe <snddadaya@gmail.com>
2025-08-28 14:32:29 +0400

    Созданы Pydantic схемы для оценок Evaluation

---
+++ src/app/schemas/evaluation.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+from src.app.models.evaluation import EvaluationGrade
+
+
+class EvaluationBase(BaseModel):
+    """Базовая схема оценок"""
+    grade: EvaluationGrade
+    comment: str | None = None
+    manager_id: int
+    user_id: int
+    task_id: int
+
+
+class EvaluationCreate(EvaluationBase):
+    """Схема для создания оценки"""
+    pass
+
+
+class EvaluationRead(EvaluationBase):
+    """Схема для получения данных оценки"""
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class EvaluationUpdate(BaseModel):
+    """Схема для обновления данных оценки"""
+    grade: Optional[EvaluationGrade] = None
+    comment: Optional[str] = None
