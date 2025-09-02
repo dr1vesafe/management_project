39d1ee7a14acb87113761e7f66faec1e03889c9d
dr1vesafe <snddadaya@gmail.com>
2025-09-02 12:57:01 +0400

    Созданы методы для получения задач для команды, изменения задачи и удаления задачи

--- src/app/services/task_crud.py
+++ src/app/services/task_crud.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
 from src.app.models.task import Task
-from src.app.schemas.task import TaskCreate
+from src.app.schemas.task import TaskCreate, TaskUpdate
 
 
 async def create_task(db: AsyncSession, task_data: TaskCreate) -> Task:
@@ -18,3 +18,24 @@ async def get_task(db: AsyncSession, task_id: int) -> Task | None:
     """Получить задачу по id"""
     result = await db.execute(select(Task).where(Task.id == task_id))
     return result.scalars().first()
+
+
+async def get_tasks_by_team(db: AsyncSession, team_id: int) -> list[Task]:
+    """Получить список задач для команды"""
+    result = await db.execute(select(Task).where(Task.team_id == team_id))
+    return result.scalars().all()
+
+
+async def update_task(db: AsyncSession, task: Task, task_data: TaskUpdate) -> Task:
+    """Изменить задачу"""
+    for field, value in task_data.dict(exclude_unset=True).items():
+        setattr(task, field, value)
+    await db.commit()
+    await db.refresh(task)
+    return task
+
+
+async def delete_task(db: AsyncSession, task: Task) -> None:
+    """Удалить задачу"""
+    await db.delete(task)
+    await db.commit()
