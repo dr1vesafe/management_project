4438a3fbe0acae379b1c11792522b6426bbd4a34
dr1vesafe <snddadaya@gmail.com>
2025-09-03 16:23:38 +0400

    Созданы маршруты для получения списка всех встреч и получения встречи по id

--- src/app/routers/meetings.py
+++ src/app/routers/meetings.py
@@ -10,6 +10,28 @@ from src.app.models.user import User
 router = APIRouter(prefix='/meetings', tags=['meetings'])
 
 
+async def check_meeting(
+        db: AsyncSession,
+        meeting_id: int,
+        user: User
+):
+    """Общая функция для проверки встречи"""
+    meeting = await meeting_crud.get_meeting(db, meeting_id)
+    if not meeting:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Встреча не найдена'
+        )
+    
+    if user.team_id != meeting.team_id and user.role != 'admin':
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='Недостаточно прав'
+        )
+    
+    return meeting
+
+
 @router.post('/', response_model=MeetingRead)
 async def create_meeting(
     meeting_data: MeetingCreate,
@@ -17,7 +39,7 @@ async def create_meeting(
     user: User = Depends(require_role('manager', 'admin'))
 ):
     """
-    Создание встречи
+    Создать встречу
     (доступно только менеджерам и админам)
     """
     if user.team_id != meeting_data.team_id and user.role != 'admin':
@@ -27,3 +49,27 @@ async def create_meeting(
         )
     
     return await meeting_crud.create_meeting(db, meeting_data)
+
+
+@router.get('/', response_model=list[MeetingRead])
+async def get_all_meetings(
+    db: AsyncSession = Depends(get_db),
+    _: User = Depends(require_role('admin'))
+):
+    """
+    Получить список всех встреч
+    (доступно только админам)
+    """
+    return await meeting_crud.get_all_meetings(db)
+
+
+@router.get('/{meeting_id}', response_model=MeetingRead)
+async def get_meeting_by_id(
+    meeting_id: int,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user)
+):
+    """Получить встречу по id"""
+    meeting = await check_meeting(db, meeting_id, user)
+
+    return meeting
