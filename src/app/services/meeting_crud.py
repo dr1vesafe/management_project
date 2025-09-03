451a6b83cf5b53151f76521099402afc4498ffa8
dr1vesafe <snddadaya@gmail.com>
2025-09-03 15:58:31 +0400

    Созданы методы для изменения и удаления встречи

--- src/app/services/meeting_crud.py
+++ src/app/services/meeting_crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
-from src.app.schemas.meeting import MeetingCreate
+from src.app.schemas.meeting import MeetingCreate, MeetingUpdate
 from src.app.models.meeting import Meeting
 from src.app.models.meeting_participants import MeetingParticipant
 
@@ -38,3 +38,43 @@ async def get_all_meetings(db: AsyncSession) -> list[Meeting]:
     """Получить список всех встреч"""
     result = await db.execute(select(Meeting))
     return result.scalars().all()
+
+
+async def update_meeting(
+        db: AsyncSession,
+        meeting: Meeting,
+        meeting_data: MeetingUpdate
+) -> Meeting:
+    """Изменение встречи"""
+    data = meeting_data.dict(exclude_unset=True)
+
+    if 'participants' in data:
+        participants_data = data.pop('participants')
+        await db.execute(
+            select(MeetingParticipant).where(MeetingParticipant.meeting_id == meeting.id)
+        )
+
+        await db.execute(
+            MeetingParticipant.__table__.delete().where(MeetingParticipant.meeting_id == meeting.id)
+        )
+
+        if participants_data:
+            for p in participants_data:
+                participant_obj = MeetingParticipant(
+                    meeting_id=meeting.id,
+                    user_id=p.user_id
+                )
+                db.add(participant_obj)
+
+    for field, value in data.items():
+        setattr(meeting, field, value)
+
+    await db.commit()
+    await db.refresh(meeting)
+    return meeting
+
+
+async def delete_meeting(db: AsyncSession, meeting: Meeting) -> None:
+    """Удалить встречу"""
+    await db.delete(meeting)
+    await db.commit()
